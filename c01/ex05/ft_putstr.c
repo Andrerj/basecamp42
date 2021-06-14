95dc4b86f465f35276578c6d5671fd80c7f63f89
andrrodr <andrrodr@student.42sp.org.br>
2021-06-14 23:31:52 +0000

    Story 17: ex05 done

---
+++ c01/ex05/ft_putstr.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_putsr(char *str)
+{
+	
+	while (*str != '\0') {
+		write(1, str, 1);
+		str++;
+	}
+
+
+
+}
+
+int		main(void)
+{
+	char *str;
+
+	str = "Aloha";
+
+	ft_putsr(str);
+	return (0);
+}
